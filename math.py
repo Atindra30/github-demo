fc90982da90191db71e308032af11e77816a322e
Atindra30 <98600328+Atindra30@users.noreply.github.com>
2022-01-29 13:51:55 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#Add implementation
+def add(x,y)
+	pass
+
+#Substract implementation
+def sub(x,y)
+	pass
+
+#Multipliy implementation
+def multiply(x,y)
+	pass
+
+#Divide implementation
+def divide(x,y)
+	pass
